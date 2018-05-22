0dd0f88f3ed9be999c29c66246967759c65eb952
Antonio de la Torre <antondelatorre@gmail.com>
2018-05-22 23:04:18 +0200

    Substitute trivial substraction.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -55,7 +55,7 @@ class GildedRose {
                         }
                     }
                 } else {
-                    item.quality = item.quality - item.quality;
+                    item.quality = 0;
                 }
             } else {
                 if (item.quality < MAX_QUALITY) {
