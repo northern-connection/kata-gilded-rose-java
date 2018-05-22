7754cc3d10807b954d37e062f749b90a9fb674ca
Antonio de la Torre <antondelatorre@gmail.com>
2018-05-22 22:43:56 +0200

    Ignore. Reformat code.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,91 +1,91 @@
 package com.gildedrose;
 
 class GildedRose {
-	public static final int MIN_QUALITY = 0;
-	public static final int MAX_QUALITY = 50;
-	Item[] items;
+    public static final int MIN_QUALITY = 0;
+    public static final int MAX_QUALITY = 50;
+    Item[] items;
 
-	public GildedRose(Item[] items) {
-		this.items = items;
-	}
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
 
-	public void updateQuality() {
-		for (int i = 0; i < items.length; i++) {
-			updateQualityItem(items[i]);
-		}
-	}
+    public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            updateQualityItem(items[i]);
+        }
+    }
 
-	private void updateQualityItem(Item item) {
-		if (!isAgedBrie(item) && !isBackstage(item)) {
+    private void updateQualityItem(Item item) {
+        if (!isAgedBrie(item) && !isBackstage(item)) {
             if (!isSulfuras(item)) {
                 if (item.quality > MIN_QUALITY) {
                     item.quality = decreaseQualityByOne(item);
                 }
             }
-		} else {
-			if (item.quality < MAX_QUALITY) {
-				item.quality = increaseQualityByOne(item);
+        } else {
+            if (item.quality < MAX_QUALITY) {
+                item.quality = increaseQualityByOne(item);
 
-				if (isBackstage(item)) {
-					if (item.sellIn < 11) {
-						if (item.quality < MAX_QUALITY) {
-							item.quality = increaseQualityByOne(item);
-						}
-					}
+                if (isBackstage(item)) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < MAX_QUALITY) {
+                            item.quality = increaseQualityByOne(item);
+                        }
+                    }
 
-					if (item.sellIn < 6) {
-						if (item.quality < MAX_QUALITY) {
-							item.quality = increaseQualityByOne(item);
-						}
-					}
-				}
-			}
-		}
+                    if (item.sellIn < 6) {
+                        if (item.quality < MAX_QUALITY) {
+                            item.quality = increaseQualityByOne(item);
+                        }
+                    }
+                }
+            }
+        }
 
-		if (!isSulfuras(item)) {
-			item.sellIn = decreaseStockByOne(item);
-		}
+        if (!isSulfuras(item)) {
+            item.sellIn = decreaseStockByOne(item);
+        }
 
-		if (item.sellIn < 0) {
-			if (!isAgedBrie(item)) {
-				if (!isBackstage(item)) {
-					if (item.quality > MIN_QUALITY) {
-						if (!isSulfuras(item)) {
-							item.quality = decreaseQualityByOne(item);
-						}
-					}
-				} else {
-					item.quality = item.quality - item.quality;
-				}
-			} else {
-				if (item.quality < MAX_QUALITY) {
-					item.quality = increaseQualityByOne(item);
-				}
-			}
-		}
-	}
+        if (item.sellIn < 0) {
+            if (!isAgedBrie(item)) {
+                if (!isBackstage(item)) {
+                    if (item.quality > MIN_QUALITY) {
+                        if (!isSulfuras(item)) {
+                            item.quality = decreaseQualityByOne(item);
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality;
+                }
+            } else {
+                if (item.quality < MAX_QUALITY) {
+                    item.quality = increaseQualityByOne(item);
+                }
+            }
+        }
+    }
 
-	private int decreaseStockByOne(Item item) {
-		return item.sellIn - 1;
-	}
+    private int decreaseStockByOne(Item item) {
+        return item.sellIn - 1;
+    }
 
-	private int increaseQualityByOne(Item item) {
-		return item.quality + 1;
-	}
+    private int increaseQualityByOne(Item item) {
+        return item.quality + 1;
+    }
 
-	private int decreaseQualityByOne(Item item) {
-		return item.quality - 1;
-	}
+    private int decreaseQualityByOne(Item item) {
+        return item.quality - 1;
+    }
 
-	private boolean isSulfuras(Item item) {
+    private boolean isSulfuras(Item item) {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
 
     private boolean isBackstage(Item item) {
-		return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-	}
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
 
-	private boolean isAgedBrie(Item item) {
-		return item.name.equals("Aged Brie");
-	}
+    private boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
+    }
 }
